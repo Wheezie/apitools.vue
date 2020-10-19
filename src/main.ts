75c30e7996ae0be0183a2e2831558c69f8699a8b
Wraptor <wraptor@wheezie.be>
2020-10-20 01:31:27 +0200

    Current Login WIP

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+
 import './registerServiceWorker'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import App from './App.vue'
+
+createApp(App)
+    .use(router)
+    .mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Login :success="{path: ''}" :showForgot="true"></Login>
   </div>
 </template>
 
+<style lang="scss" scoped>
+  #login-holder {
+    width: 300px;
+  }
+</style>
+
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Login from '@/components/Login.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    Login,
   },
 })
 export default class Home extends Vue {}
